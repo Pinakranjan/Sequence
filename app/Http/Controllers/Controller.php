a0f4ab55e2de26219d408873ad459d6b7430a8d2
Pinak Ranjan Sahoo <pinak_hits@hotmail.com>
2026-02-17 19:43:24 +0530

    Swagger documentation updated

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -20,6 +20,14 @@ namespace App\Http\Controllers;
  *      url=L5_SWAGGER_CONST_HOST,
  *      description="API Server"
  * )
+ *
+ * @OA\SecurityScheme(
+ *     securityScheme="sanctum",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     description="Enter the Bearer token obtained from POST /api/auth/login. Example: 1|abc123..."
+ * )
  */
 abstract class Controller
 {
